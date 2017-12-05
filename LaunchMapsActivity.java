fad6504f93e66c636c51e0db75160f1705381131
as063LAUNCH <as063@bucknell.edu>
2017-12-04 19:17:26 -0500

    launch google maps basic UI and phone launch

---
+++ LaunchMapsActivity.java
@@ -0,0 +1,13 @@
+package com.example.anushikhasharma.childsplay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchMapsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.launch_maps);
+    }
+}
